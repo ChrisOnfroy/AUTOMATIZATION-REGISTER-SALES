2f4cfba5843e4b04042acc6e8c4ae23f703382b2
Cristian Morales <cmoralesavendano6@gmail.com>
2026-03-10 18:56:59 -0500

    Diagrama de flujo y le damos inicio al programa

---
+++ app.py
@@ -0,0 +1,8 @@
+#this is to principal file of the project, it will run the app
+
+
+print("Welcome to the app!")
+
+print("----------------------------------")
+print("|  AUTOMATIZATION REGISTER SALES  |")
+print("----------------------------------")
